646e3ea01d3370e8b3ed450166124d1d2bbf5afe
aryapromotora-alt <aryapromotora@gmail.com>
2025-09-10 11:07:38 -0300

    Add files via upload

---
+++ src/models/user.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Instância global do banco
+db = SQLAlchemy()
+
+# Exemplo de tabela de usuários
+class User(db.Model):
+    __tablename__ = "users"  # nome da tabela no banco
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
